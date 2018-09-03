60cbd989cdaaf853a86e758f0007dbfd58268801
Mikael Modig <mail@mikaelmodig.se>
2018-09-03 20:16:07 +0200

    Styling

--- src/FlowerCard/FlowerCard.js
+++ src/FlowerCard/FlowerCard.js
@@ -19,9 +19,12 @@ class FlowerCard extends Component {
     const { name } = this.props;
     return (
       <div className="flowerCardContainer" onClick={this.handleFlowerCardClick}>
+      <img src="https://www.istockphoto.com/se/foto/anemone-blommor-i-blom-gm845538942-138594797"></img>
         {name}
-        <button>Watered</button>
-        <button onClick={this.deleteFlower}>Delete</button>
+        <div>
+          <button className="flowerCardButton">Watered</button>
+          <button className="flowerCardButton" onClick={this.deleteFlower}>Delete</button>
+        </div>
       </div>
     );
   }
