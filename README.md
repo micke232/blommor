6affe84905806d1c8afff0dd808d1fc33210cc0a
Mikael Modig <mikael232@hotmail.com>
2018-09-11 17:58:50 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#Vattna blommor app :)
+# App that keeps your flowers hydrated
+## Front end is made with App Create App
+## Backend is Firebase 
--- src/RoomCard/RoomCard.js
+++ src/RoomCard/RoomCard.js
@@ -8,7 +8,6 @@ class RoomCard extends Component {
     onRoomClicked(name)
   }
 
-
   render() {
     const { name, color } = this.props;
     return (
